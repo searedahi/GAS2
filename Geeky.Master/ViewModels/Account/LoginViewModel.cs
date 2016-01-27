03faca0f2f466120379d971a447920d695fa9a63
searedahi <sborbust@gmail.com>
2016-01-26 20:22:41 -0800

    Were using username now.  Dont like exposing email anymore.

--- Geeky.Master/ViewModels/Account/LoginViewModel.cs
+++ Geeky.Master/ViewModels/Account/LoginViewModel.cs
@@ -9,8 +9,7 @@ namespace Geeky.Master.ViewModels.Account
     public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
