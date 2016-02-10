a259bf1b76985c3e3129eb08fd05770a03f68bed
searedahi <sborbust@gmail.com>
2016-02-10 12:29:06 -0800

    swimteam user from bash

---
+++ Geeky.Swimteam/Models/SwimteamUser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Geeky.Models.Base;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Geeky.Swimteam.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class SwimteamUser : IdentityUser
+    {
+        public virtual ICollection<UserProfile> Profiles { get; set; }
+        public virtual ICollection<PhoneNumber> PhoneNumbers { get; set; }
+        public virtual ICollection<PhysicalAddress> Addresses { get; set; }
+
+        //[Display(Name = "Patient Id")]
+        //public string PatientId { get; set; }
+        //public GeekyUser Caregiver { get; set; }
+        //public ICollection<GeekyUser> Physicians { get; set; }
+
+        public SwimteamUser()
+        {
+            Profiles = new List<UserProfile>();
+            Addresses = new List<PhysicalAddress>();
+            PhoneNumbers = new List<PhoneNumber>();
+        }
+    }
+}
