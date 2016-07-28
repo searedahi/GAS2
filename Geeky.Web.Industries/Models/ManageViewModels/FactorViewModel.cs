b6fcf3b70ea39d6a561846d7d2d8febc99a262e4
searedahi <moneyhamachi@gmail.com>
2016-07-28 09:42:14 -0700

    New .Net Core project added.

---
+++ Geeky.Web.Industries/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geeky.Web.Industries.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
