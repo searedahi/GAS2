4a35418ed10cee2341e1d908316c6c9f33d715e6
searedahi <searedahi@GEEKYLAPTOP>
2016-08-02 18:24:01 -0700

    Got the auth server building

---
+++ Geeky.Web.Auth/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geeky.Web.Auth.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
