d9ba32325426b6325f5ccec44bacab630c706cdc
searedahi <moneyhamachi@gmail.com>
2016-08-17 16:30:43 -0700

    From the office now

---
+++ Geeky.Web.Auth.Test/LoginTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Geeky.Web.Auth.Test
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void LoginWorking()
+        {
+
+            var wc = new WebClient();
+            byte[] resultBytes = {};
+
+            using (wc)
+            {
+                resultBytes = wc.DownloadData("https://localhost:44364/");
+            }
+
+            Assert.IsNotNull(wc);
+            Assert.IsNotNull(resultBytes);
+
+        }
+    }
+}
